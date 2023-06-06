36fa3002ff76337b960380ed3b481f1b917accad
Anam0111 <123912096+Anam0111@users.noreply.github.com>
2023-06-06 20:52:56 +0700

    Update Employee.py
    Menambahkan Kelas Employee

--- Employee.py
+++ Employee.py
@@ -1,20 +1,20 @@
-class employee:
+class Employee:
   empCount = 0
   
-  def __init__ (Self,name,salary):
+  def __init__ (self,name,salary):
     self.name= name
     self.salary = salary
-    employee
+    Employee.empCount +=1
    
-  def displayCount (self):
+  def displayCount(self):
     print("Total Employee %d" % Employee.empCount)
     
-  def displayCount (self):
-    print ("Name :",self.name, ", Salary: ", self.salary)
+  def displayEmployee(self):
+    print("Name :",self.name, ", Salary: ", self.salary)
     
-empl = Employee("Anam", 10000)
-emp2 = Employee("Anam", 10000)
+emp1 = Employee("Anam", 10000)
+emp2 = Employee("siti", 5000)
 emp1.displayEmployee()
 emp2.displayEmployee()
-print ("Total Employee %d" % Employee.empCount)
+print("Total Employee %d" % Employee.empCount)
   
