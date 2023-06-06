47437d78ebbabd18636886c9652245c2878fbc63
Anam0111 <123912096+Anam0111@users.noreply.github.com>
2023-06-06 20:34:19 +0700

    Create Employee.py
    Menambahkan kelas Employee

---
+++ Employee.py
@@ -0,0 +1,20 @@
+class employee:
+  empCount = 0
+  
+  def __init__ (Self,name,salary):
+    self.name= name
+    self.salary = salary
+    employee
+   
+  def displayCount (self):
+    print("Total Employee %d" % Employee.empCount)
+    
+  def displayCount (self):
+    print ("Name :",self.name, ", Salary: ", self.salary)
+    
+empl = Employee("Anam", 10000)
+emp2 = Employee("Anam", 10000)
+emp1.displayEmployee()
+emp2.displayEmployee()
+print ("Total Employee %d" % Employee.empCount)
+  
